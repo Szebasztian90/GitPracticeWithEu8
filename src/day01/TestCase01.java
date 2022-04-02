d608d54ac222cfd452921b26a4e987ffa5818f0c
Sebastian Dulau <dulausebastian@gmail.com>
2022-04-02 21:10:07 +0100

    Fourth Commit

--- src/day01/TestCase01.java
+++ src/day01/TestCase01.java
@@ -3,12 +3,7 @@ package day01;
 public class TestCase01 {
     public static void main(String[] args) {
 
-        System.out.println("Testing with Chrome Browser");
-        System.out.println("out");
-
-        System.out.println("Testing with Edge");
-
-        System.out.println("Test Test");
-
+        System.out.println("hello world");
     }
+
 }
